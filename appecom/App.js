0407f475432b32b5f2e621b163af9f36af5b2e60
Danilo Carvalho <danilo.carvalho@inatel.br>
2020-11-14 13:20:46 -0300

    Add commands and dependencies

--- appecom/App.js
+++ appecom/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AppECom from './src/index';
+import AppECom from './src/index.js';
 
 export default function App() {
   return (
--- appecom/src/index.js
+++ appecom/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
 
 const AppECom = () => {
     return (
-        <View>DC</View>
+        <View></View>
     );
 }
 
